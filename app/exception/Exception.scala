92da0b8177d0b1e57b487308786babdd1208c438
bgowda <bindiya.r@gmail.com>
2012-09-05 17:19:41 +0100

    Fixed test cases

---
+++ app/exception/Exception.scala
@@ -0,0 +1,10 @@
+package exception
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bindiya.jinnappa
+ * Date: 05/09/2012
+ * Time: 14:37
+ * To change this template use File | Settings | File Templates.
+ */
+class DuplicateException(message:String) extends RuntimeException
